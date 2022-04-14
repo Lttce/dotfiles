a2dbc171c537c53ad7281917a47f0137210a5651
Lttce <Lettuce1248@gmail.com>
2022-04-14 23:35:34 +0900

    Add: nvim-cmp keys
    because, nvim-cmp was changed set no default keys by update.

--- nvim/plugins/cmp.lua
+++ nvim/plugins/cmp.lua
@@ -11,6 +11,8 @@ cmp.setup({
         end,
     },
     mapping = {
+        ['<C-p>'] = cmp.mapping.select_prev_item(),
+        ['<C-n>'] = cmp.mapping.select_next_item(),
         ['<CR>'] = cmp.mapping.confirm({ select = true }),
     },
     sources = cmp.config.sources({
