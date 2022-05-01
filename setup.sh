3fa6d5e22e2a8447c682d27418a522d65f5f0570
Lttce <Lettuce1248@gmail.com>
2022-05-01 17:55:07 +0900

    Change: setup shell script

---
+++ setup.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+set -e
+
+#
+# apt package
+#
+
+sudo apt update -y 
+sudo apt upgrade -y
+
+sudo apt install build-essential -y
+
+#
+# Homebrew
+#
+
+if !(which brew > /dev/null 2>&1); then
+    NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/$USER/.profile
+    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+fi
+
+if [ -e ~/dotfiles/Brewfile ]; then
+    brew bundle --file ~/dotfiles/brew/Brewfile
+fi
+
+#
+# Symbolic links
+#
+
+if [ -d ~/dotfiles ]; then
+    if [ ! -d ~/.config ]; then
+        mkdir -p ~/.config
+    fi
+
+    if [ ! -L ~/.config/nvim ]; then
+        ln -s ~/dotfiles/nvim ~/.config
+    fi
+
+    if [ ! -L ~/.vimrc ]; then
+        ln -s ~/dotfiles/vim/vimrc ~/.vimrc
+    fi
+
+    if [ ! -L ~/.tmux.conf ]; then
+        ln -s ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
+    fi
+fi
+
+# relogin shell
+exec $SHELL -l
+
