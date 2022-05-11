a13e39dbce580adcff374106e96dbd354c8f7eaf
Lttce <Lettuce1248@gmail.com>
2022-05-11 22:34:31 +0900

    Add: lsp assistance plugins

--- nvim/plugins/cmp.lua
+++ nvim/plugins/cmp.lua
@@ -13,12 +13,25 @@ cmp.setup({
     mapping = {
         ['<C-p>'] = cmp.mapping.select_prev_item(),
         ['<C-n>'] = cmp.mapping.select_next_item(),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        ['<CR>'] = cmp.mapping.confirm(),
     },
     sources = cmp.config.sources({
         { name = 'nvim_lsp' },
         { name = 'vsnip' },
+    },{
         { name = 'buffer' },
-    })
+    },{
+        { name = 'nvim_lsp_signature_help' },
+    }),
+    window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+    },
+})
+
+cmp.setup.cmdline('/', {
+    sources = {
+        { name = 'nvim_lsp_document_symbol' },
+    },
 })
 
