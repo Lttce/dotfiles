58f38ca0769ae747f322f04b0571cbe39ec4ffda
Lttce <Lettuce1248@gmail.com>
2022-05-18 19:14:41 +0900

    Update: setup.sh

--- setup.sh
+++ setup.sh
@@ -16,12 +16,13 @@ sudo apt install build-essential -y
 #
 
 if !(which brew > /dev/null 2>&1); then
-    NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    NONINTERACTIVE=1
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/$USER/.profile
     eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 fi
 
-if [ -e ~/dotfiles/Brewfile ]; then
+if [ -e ~/dotfiles/brew/Brewfile ]; then
     brew bundle --file ~/dotfiles/brew/Brewfile
 fi
 
@@ -38,11 +39,11 @@ if [ -d ~/dotfiles ]; then
         ln -s ~/dotfiles/nvim ~/.config
     fi
 
-    if [ ! -L ~/.vimrc ]; then
+    if [ ! -L ~/vim/.vimrc ]; then
         ln -s ~/dotfiles/vim/vimrc ~/.vimrc
     fi
 
-    if [ ! -L ~/.tmux.conf ]; then
+    if [ ! -L ~/tmux/.tmux.conf ]; then
         ln -s ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
     fi
 fi
