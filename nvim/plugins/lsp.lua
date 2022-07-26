13430c20782ab766ac07343f99808840af2ceb35
Lttce <31960436+Lttce@users.noreply.github.com>
2022-07-26 16:21:20 +0900

    Replace: nvim-lsp-installer to mason.nvim

--- nvim/plugins/lsp.lua
+++ nvim/plugins/lsp.lua
@@ -1,32 +1,16 @@
 -- nvim-lsp
-
-local on_attach = function(client, bufnr)
-    local function buf_set_keymap(...)
-        vim.api.nvim_buf_set_keymap(bufnr, ...)
-    end
-
-    local opts = { noremap = true, silent = true }
-    buf_set_keymap('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
-    buf_set_keymap('n', '<space>d', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
-    buf_set_keymap('n', '<space>r', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-    buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-    buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-end
-
-local lsp_installer = require('nvim-lsp-installer')
-
-lsp_installer.on_server_ready(
-    function(server)
+require('mason-lspconfig').setup_handlers {
+    function(server_name)
         local opts = {}
 
         -- Config each servers
         -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
 
-        if server.name == 'emmet_ls' then
+        if server_name == 'emmet_ls' then
             opts.filetypes = { 'html', 'css', 'typescriptreact' }
         end
 
-        if server.name == 'sumneko_lua' then
+        if server_name == 'sumneko_lua' then
             opts.settings = {
                 Lua = {
                     diagnostics = {
@@ -36,8 +20,24 @@ lsp_installer.on_server_ready(
             }
         end
 
-        opts.on_attach = on_attach
+        -- Key mapping
+        opts.on_attach = function(_, bufnr)
+            local function buf_set_keymap(...)
+                vim.api.nvim_buf_set_keymap(bufnr, ...)
+            end
+
+            local buf_opts = { noremap = true, silent = true }
+            buf_set_keymap('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', buf_opts)
+            buf_set_keymap('n', '<space>d', '<cmd>lua vim.diagnostic.open_float()<CR>', buf_opts)
+            buf_set_keymap('n', '<space>r', '<cmd>lua vim.lsp.buf.references()<CR>', buf_opts)
+            buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', buf_opts)
+            buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', buf_opts)
+        end
+
+        -- enable nvim-cmp 
         opts.update_capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
-        server:setup(opts)
+
+        -- launch server
+        require('lspconfig')[server_name].setup(opts)
     end
-)
+}
