cb882d7af103af0d591443abcd0b10c922ab49be
Lttce <31960436+Lttce@users.noreply.github.com>
2026-07-15 03:39:48 +0900

    feat: add initial neovim config

---
+++ nvim/init.lua
@@ -0,0 +1,33 @@
+-- style
+
+vim.opt.termguicolors = true
+
+-- edit
+
+vim.keymap.set('i', 'jk', '<esc>')
+
+-- 組み込みプラグインを無効化
+-- vim.g.loaded_netrw = 1
+-- vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_gzip = 1
+vim.g.loaded_zip = 1
+vim.g.loaded_zipPlugin = 1
+vim.g.loaded_tar = 1
+vim.g.loaded_tarPlugin = 1
+vim.g.loaded_getscript = 1
+vim.g.loaded_getscriptPlugin = 1
+vim.g.loaded_vimball = 1
+vim.g.loaded_vimballPlugin = 1
+vim.g.loaded_2html_plugin = 1
+vim.g.loaded_logipat = 1
+vim.g.loaded_rrhelper = 1
+vim.g.loaded_spellfile_plugin = 1
+vim.g.loaded_matchit = 1
+vim.g.loaded_tutor_mode_plugin = 1
+
+-- provider を無効化
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_node_provider = 0
+vim.g.loaded_perl_provider = 0
+
